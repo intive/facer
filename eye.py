1f45c3345524eeef139e464474f89a3ee5849cb1
Wiktor Rutka <wrutka@gmail.com>
2013-12-16 23:53:28 +0100

    [skin] hsv skin detection
     * categorizied previous approach as rgb
     * delegate to functions

--- eye.py
+++ eye.py
@@ -11,32 +11,80 @@ if vc.isOpened(): # try to get the first frame
 else:
     rval = False
 
-def create_r_mapper(r):
-    up = -1.3767*(r*r) + 1.0743*r + 0.1452
-    down = -0.776*(r*r) + 0.5601*r + 0.1766
-    return (up, down)
 
-while rval:
-    cv2.imshow("preview", frame)
-    rval, frame = vc.read()
+def hsv_param_skindetection(frame):
+    proc_f = cv2.cvtColor(frame, cv2.COLOR_RGB2HSV)
+    mapr = proc_f[:,:,0]>=90
+    mapr *= proc_f[:,:,0]<=200
+    mapr *= proc_f[:,:,1]>=60
+    mapr *= proc_f[:,:,1]<=200
+    mapr *= proc_f[:,:,2]>=30
+    mapr *= proc_f[:,:,2]<=255
+    frame[:,:,0] = frame[:,:,0]*mapr
+    frame[:,:,1] = frame[:,:,1]*mapr
+    frame[:,:,2] = frame[:,:,2]*mapr
+    return frame
 
-    proc_f = numpy.array(frame, dtype=numpy.float64)
-    proc_f /= 256
-    r = proc_f[:, :, 2] / (proc_f[:, :, 0] + proc_f[:, :, 1] + proc_f[:, :, 2])
+def rgb_param_skindetection(frame):
+    def create_r_mapper(r):
+        up = -1.8423*(r*r) + 1.5294*r + 0.0422
+        down = -0.7279*(r*r) + 0.6066*r + 0.1766
+        return (up, down)
+
+    proc_f = frame.astype(float)
+    proc_f /= 255
+    r = frame[:, :, 2] / (frame[:, :, 0] + frame[:, :, 1] + frame[:, :, 2])
     (up, down) = create_r_mapper(r)
 
-
-    g = proc_f[:, :, 1] / (proc_f[:, :, 0] + proc_f[:, :, 1] + proc_f[:, :, 2])
+    g = frame[:, :, 1] / (frame[:, :, 0] + frame[:, :, 1] + frame[:, :, 2])
     mapr = g<=up
     mapr *= mapr>=down
 
     b = numpy.arange(frame.shape[0] * frame.shape[1])\
              .reshape(frame.shape[0], frame.shape[1])
     b.fill(0)
+    #g *= mapr
+    #g *= 255
+    #g = g.astype(numpy.uint8)
+    #r *= mapr
+    #r *= 255
+    #r = g.astype(numpy.uint8)
 
-    frame[:, :, 2] *= mapr
-    frame[:, :, 1] *= mapr
-    frame[:, :, 0] *= mapr
+    frame[:,:,0] = b.astype(numpy.uint8)
+    frame[:,:,1] *= mapr
+    frame[:,:,2] *= mapr
+
+    return frame
+
+while rval:
+    cv2.imshow("preview", frame)
+
+    rval, frame = vc.read()
+    frame = hsv_param_skindetection(frame)
+    #frame = rgb_param_skindetection(frame)
+    #frame[:,:,0] = r
+    #frame[:,:,1] = g
+    #frame[:,:,2] = b
+    #frame.astype(numpy.uint8)
+
+    #frame[:, :, 2] *= mapr
+    #frame[:, :, 1] *= mapr
+    #frame[:, :, 0] *= mapr
+    #frame = frame.astype(int)
+    #frame.fill(122.201)
+    #frame = numpy.array(frame, dtype=numpy.integer)
+    #proc_f = proc_f.astype(float)
+    #proc_f[:, :, 2] = 1 - proc_f[:, :, 2]
+    #frame = ((255 - frame[:,:,2]) + frame[:,:,1])/2
+
+    #proc_f = proc_f.astype(float)
+    #proc_f = proc_f.astype(int)
+    #print proc_f[1,1,:]
+    #frame = proc_f
+    #print frame.shape
+    #frame = ((proc_f[:, :, 0] + proc_f[:, :, 2])/2)
+    #frame[:, :, 1]
+    #frame[:, :, 0]
 
     key = cv2.waitKey(10)
     if key == 27: # exit on ESC
